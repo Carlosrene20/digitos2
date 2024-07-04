3796761857b0eb154663def617f9e3516038a4b4
Carlosrene20 <senesy23@gmail.com>
2024-07-04 16:35:17 -0500

    first commit

---
+++ R/hello.R
@@ -0,0 +1,16 @@
+digitos2 <- function(Numero_3_digitos){
+  N<- Numero_3_digitos
+  if(N>=100 & N < 1000){
+    dig1<- trunc(N/100)
+    x1 <-N%% 100
+    dig2 <- trunc(x1/10)
+    dig3 <- x1%%10
+    s <- dig1+dig2+dig3
+    print("LA SUMA DE LOS DIGITOS ES :")
+    print(s)
+    else{
+      print("no es un numero de 3 digitos ")
+    }
+
+  }
+}
